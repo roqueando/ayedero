e769eaf689f36a79d4fe1f13a800252d98d5663b
Vitor Roque <vitor.roquep@gmail.com>
2023-06-09 21:08:39 -0300

    fix: adding installation of neovim and tmux

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Installing Neovim [Ayedero] ..."
+
+ln -s ~/ayedero/nvim ~/.config/nvim
+
+echo "Neovim [Ayedero] Installed!"
+
+echo "Installing Tmux Config [Ayedero]"
+
+ln -s ~/ayedero/.tmux.conf ~/.tmux.conf
+
+echo "Tmux Config [Ayedero] Installed!"
