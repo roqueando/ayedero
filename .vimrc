45f71c94ada42a2047ff9304f218a4fbd1d2c4d9
Vitor Roque <vitor.roquep@gmail.com>
2023-10-19 11:39:17 -0300

    feat: adding vim again

---
+++ .vimrc
@@ -0,0 +1,12 @@
+set nocompatible
+filetype plugin indent on
+syntax on
+
+set relativenumber
+
+inoremap jk <esc>
+
+let mapleader = ";"
+
+nnoremap <leader>w :w<cr>
+nnoremap <leader>e :Lexplore<cr>
--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ echo "Installing Tmux [...]"
 ln -s ~/ayedero/.tmux.conf ~/.tmux.conf
 
 echo "Tmux [OK]"
+
+echo "Installing Neovim & VIM [...]"
+ln -s $HOME/ayedero/.vimrc $HOME/.config/nvim/init.vim
+ln -s $HOME/ayedero/.vimrc $HOME/.vimrc
+
+echo "Neovim & VIM [OK]"
